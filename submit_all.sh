a57a3f6646f5d01729f95e39b48f3ab460c48687
Will M. Farr <wfarr-vscholar@flatironinstitute.org>
2018-09-05 19:36:42 -0400

    Updated the number of selected events to use in 1yr runs.  Go!

--- submit_all.sh
+++ submit_all.sh
@@ -6,8 +6,8 @@ source ~/.bashrc
 export PYTHONPATH="$PYTHONPATH:/mnt/home/wfarr/PISNLineCosmography"
 
 # One-year runs
-sbatch -n 1 --cpus-per-task 4 -p cca -o logs/1yr_128.out -e logs/1yr_128.err fit.py --sampfile parameters_1yr.h5 --samp 128 --selfile selected.h5 --chainfile population_1yr_0128.h5 --tracefile traceplot_1yr_0128.pdf
-sbatch -n 1 --cpus-per-task 4 -p cca -o logs/1yr_256.out -e logs/1yr_256.err fit.py --sampfile parameters_1yr.h5 --samp 256 --selfile selected.h5 --chainfile population_1yr_0256.h5 --tracefile traceplot_1yr_0256.pdf
+sbatch -n 1 --cpus-per-task 4 -p cca -o logs/1yr_128.out -e logs/1yr_128.err fit.py --sampfile parameters_1yr.h5 --samp 128 --selfile selected.h5 --nsel 131072 --chainfile population_1yr_0128.h5 --tracefile traceplot_1yr_0128.pdf
+sbatch -n 1 --cpus-per-task 4 -p cca -o logs/1yr_256.out -e logs/1yr_256.err fit.py --sampfile parameters_1yr.h5 --samp 256 --selfile selected.h5 --nsel 131072 --chainfile population_1yr_0256.h5 --tracefile traceplot_1yr_0256.pdf
 
 # Five year runs
 sbatch -n 1 --cpus-per-task 4 -p cca -o logs/5yr_128.out -e logs/5yr_128.err fit.py --sampfile parameters.h5 --samp 128 --selfile selected.h5 --chainfile population_5yr_0128.h5 --tracefile traceplot_5yr_0128.pdf
