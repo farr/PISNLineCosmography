296ca9b815fee89c682a66bf22dc9295a692b3ea
Will M. Farr <wfarr-vscholar@flatironinstitute.org>
2018-09-12 01:23:07 -0400

    Make the small run actually use small parameter file.

--- submit_all.sh
+++ submit_all.sh
@@ -6,7 +6,7 @@ source ~/.bashrc
 export PYTHONPATH="$PYTHONPATH:/mnt/home/wfarr/PISNLineCosmography"
 
 # Small runs
-sbatch -n 1 --cpus-per-task 4 -p cca -o logs/small.out -e logs/small.err fit.py --sampfile parameters_1yr.h5 --samp 64 --selfile selected.h5 --nsel 2048 --chainfile population_small.h5 --tracefile traceplot_small.pdf
+sbatch -n 1 --cpus-per-task 4 -p cca -o logs/small.out -e logs/small.err fit.py --sampfile parameters_small.h5 --samp 64 --selfile selected.h5 --nsel 2048 --chainfile population_small.h5 --tracefile traceplot_small.pdf
 
 # One-year runs
 sbatch -n 1 --cpus-per-task 4 -p cca -o logs/1yr.out -e logs/1yr.err fit.py --sampfile parameters_1yr.h5 --samp 64 --selfile selected.h5 --nsel 32768 --chainfile population_1yr.h5 --tracefile traceplot_1yr.pdf
