7d638bb0a0c7330e1bd119512626de70f83ededd
Will M. Farr <wfarr-vscholar@flatironinstitute.org>
2018-09-13 00:52:53 -0400

    Small runs not yet converged; doubling again.

--- submit_all.sh
+++ submit_all.sh
@@ -6,8 +6,8 @@ source ~/.bashrc
 export PYTHONPATH="$PYTHONPATH:/mnt/home/wfarr/PISNLineCosmography"
 
 # Small runs
-sbatch -n 1 --cpus-per-task 4 -p cca -o logs/small.out -e logs/small.err fit.py --sampfile parameters_small.h5 --samp 128 --selfile selected.h5 --nsel 4096 --chainfile population_small.h5 --tracefile traceplot_small.pdf
-sbatch -n 1 --cpus-per-task 4 -p cca -o logs/small2.out -e logs/small2.err fit.py --sampfile parameters_small.h5 --samp 128 --selfile selected.h5 --nsel 4096 --chainfile population_small2.h5 --tracefile traceplot_small2.pdf
+sbatch -n 1 --cpus-per-task 4 -p cca -o logs/small.out -e logs/small.err fit.py --sampfile parameters_small.h5 --samp 256 --selfile selected.h5 --nsel 8192 --chainfile population_small.h5 --tracefile traceplot_small.pdf
+sbatch -n 1 --cpus-per-task 4 -p cca -o logs/small2.out -e logs/small2.err fit.py --sampfile parameters_small.h5 --samp 256 --selfile selected.h5 --nsel 8192 --chainfile population_small2.h5 --tracefile traceplot_small2.pdf
 
 # One-year runs
 sbatch -n 1 --cpus-per-task 4 -p cca -o logs/1yr.out -e logs/1yr.err fit.py --sampfile parameters_1yr.h5 --samp 64 --selfile selected.h5 --nsel 32768 --chainfile population_1yr.h5 --tracefile traceplot_1yr.pdf
