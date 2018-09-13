bc7a69617373d067d7bf142384bb50a695efc88d
Will M. Farr <wfarr-vscholar@flatironinstitute.org>
2018-09-13 17:13:54 -0400

    Ready to start the 5year runs (50 times longer than small data set, may take 28 days!)

--- submit_all.sh
+++ submit_all.sh
@@ -14,5 +14,5 @@ sbatch -n 1 --cpus-per-task 4 -p cca -o logs/1yr.out -e logs/1yr.err fit.py --sa
 sbatch -n 1 --cpus-per-task 4 -p cca -o logs/1yr2.out -e logs/1yr2.err fit.py --sampfile parameters_1yr.h5 --samp 128 --selfile selected.h5 --nsel 65536 --chainfile population_1yr2.h5 --tracefile traceplot_1yr2.pdf
 
 # Five year runs
-sbatch -n 1 --cpus-per-task 4 -p cca -o logs/5yr.out -e logs/5yr.err fit.py --sampfile parameters.h5 --samp 64 --selfile selected.h5 --nsel 131072 --chainfile population_5yr.h5 --tracefile traceplot_5yr.pdf
-sbatch -n 1 --cpus-per-task 4 -p cca -o logs/5yr2.out -e logs/5yr2.err fit.py --sampfile parameters.h5 --samp 64 --selfile selected.h5 --nsel 131072 --chainfile population_5yr2.h5 --tracefile traceplot_5yr2.pdf
+sbatch -n 1 --cpus-per-task 4 -p cca -o logs/5yr.out -e logs/5yr.err fit.py --sampfile parameters.h5 --samp 64 --selfile selected.h5 --nsel 262144 --chainfile population_5yr.h5 --tracefile traceplot_5yr.pdf
+sbatch -n 1 --cpus-per-task 4 -p cca -o logs/5yr2.out -e logs/5yr2.err fit.py --sampfile parameters.h5 --samp 64 --selfile selected.h5 --nsel 262144 --chainfile population_5yr2.h5 --tracefile traceplot_5yr2.pdf
