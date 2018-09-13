6077f55d982e730e16dc67a2ce6b8f85dfb4488d
Will M. Farr <wfarr-vscholar@flatironinstitute.org>
2018-09-13 17:17:34 -0400

    Let's try a 50-day time limit.

--- submit_all.sh
+++ submit_all.sh
@@ -13,6 +13,6 @@ sbatch -n 1 --cpus-per-task 4 -p cca -o logs/small2.out -e logs/small2.err fit.p
 sbatch -n 1 --cpus-per-task 4 -p cca -o logs/1yr.out -e logs/1yr.err fit.py --sampfile parameters_1yr.h5 --samp 128 --selfile selected.h5 --nsel 65536 --chainfile population_1yr.h5 --tracefile traceplot_1yr.pdf
 sbatch -n 1 --cpus-per-task 4 -p cca -o logs/1yr2.out -e logs/1yr2.err fit.py --sampfile parameters_1yr.h5 --samp 128 --selfile selected.h5 --nsel 65536 --chainfile population_1yr2.h5 --tracefile traceplot_1yr2.pdf
 
-# Five year runs
-sbatch -n 1 --cpus-per-task 4 -p cca -o logs/5yr.out -e logs/5yr.err fit.py --sampfile parameters.h5 --samp 64 --selfile selected.h5 --nsel 262144 --chainfile population_5yr.h5 --tracefile traceplot_5yr.pdf
-sbatch -n 1 --cpus-per-task 4 -p cca -o logs/5yr2.out -e logs/5yr2.err fit.py --sampfile parameters.h5 --samp 64 --selfile selected.h5 --nsel 262144 --chainfile population_5yr2.h5 --tracefile traceplot_5yr2.pdf
+# Five year runs.  These are *long* (28 days estimated), so go in the general queue.
+sbatch -n 1 --cpus-per-task 4  -t 50-00:00:00 -o logs/5yr.out -e logs/5yr.err fit.py --sampfile parameters.h5 --samp 64 --selfile selected.h5 --nsel 262144 --chainfile population_5yr.h5 --tracefile traceplot_5yr.pdf
+sbatch -n 1 --cpus-per-task 4  -t 50-00:00:00 -o logs/5yr2.out -e logs/5yr2.err fit.py --sampfile parameters.h5 --samp 64 --selfile selected.h5 --nsel 262144 --chainfile population_5yr2.h5 --tracefile traceplot_5yr2.pdf
