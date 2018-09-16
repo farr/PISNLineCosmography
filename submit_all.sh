bb0fac5841a65c13df30ba5768da73e4df276b95
Will M. Farr <wfarr-vscholar@flatironinstitute.org>
2018-09-16 00:07:28 -0400

    Fix typo in submit_all.sh.

--- submit_all.sh
+++ submit_all.sh
@@ -19,7 +19,7 @@ sbatch $CCA_ARGS -o logs/small_H0_Planck_Om.out -e logs/small_H0_Planck_Om.err f
 sbatch $CCA_ARGS -o logs/1yr_free.out -e logs/1yr_free.err fit.py --sampfile parameters_1yr.h5 --samp 64 --selfile selected.h5 --nsel 65536 --chainfile population_1yr_free.h5 --tracefile traceplot_1yr_free.pdf --prior free
 sbatch $CCA_ARGS -o logs/1yr_Planck_Om_w.out -e logs/1yr_Planck_Om_w.err fit.py --sampfile parameters_1yr.h5 --samp 64 --selfile selected.h5 --nsel 65536 --chainfile population_1yr_Planck_Om_w.h5 --tracefile traceplot_1yr_Planck_Om_w.pdf --prior Planck-Om-w
 sbatch $CCA_ARGS -o logs/1yr_H0.out -e logs/1yr_H0.err fit.py --sampfile parameters_1yr.h5 --samp 64 --selfile selected.h5 --nsel 65536 --chainfile population_1yr_H0.h5 --tracefile traceplot_1yr_H0.pdf --prior H0
-sbatch $CCA_AGRS -o logs/1yr_H0_Planck_Om.out -e logs/1yr_H0_Planck_Om.err fit.py --sampfile parameters_1yr.h5 --samp 64 --selfile selected.h5 --nsel 65536 --chainfile population_1yr_H0_Planck_Om.h5 --tracefile traceplot_1yr_H0_Planck_Om.pdf --prior H0-Planck-Om
+sbatch $CCA_ARGS -o logs/1yr_H0_Planck_Om.out -e logs/1yr_H0_Planck_Om.err fit.py --sampfile parameters_1yr.h5 --samp 64 --selfile selected.h5 --nsel 65536 --chainfile population_1yr_H0_Planck_Om.h5 --tracefile traceplot_1yr_H0_Planck_Om.pdf --prior H0-Planck-Om
 
 # Five year runs.  These are *long* (28 days estimated), so go in the general queue.
 sbatch $GEN_ARGS -o logs/5yr_free.out -e logs/5yr_free.err fit.py --sampfile parameters.h5 --samp 64 --selfile selected.h5 --nsel 262144 --chainfile population_5yr_free.h5 --tracefile traceplot_5yr_free.pdf --prior free
