a2566f5816696e921265a8ee3ab587dc3c68014e
Will M. Farr <wfarr-vscholar@flatironinstitute.org>
2018-09-19 14:41:43 -0400

    Now I know what `wait` does in a script.

--- run_small.sh
+++ run_small.sh
@@ -10,7 +10,9 @@ export PYTHONPATH="$PYTHONPATH:/mnt/home/wfarr/PISNLineCosmography"
 NSAMP=64
 NSEL=8192
 
-srun --pack-group=0 fit.py --sampfile parameters_small.h5 --samp $NSAMP --selfile selected.h5 --nsel $NSEL --chainfile population_small_free.h5 --tracefile traceplot_small_free.pdf --prior free
-srun --pack-group=1 fit.py --sampfile parameters_small.h5 --samp $NSAMP --selfile selected.h5 --nsel $NSEL --chainfile population_small_Planck_Om_w.h5 --tracefile traceplot_small_Planck_Om_w.pdf --prior Planck-Om-w
-srun --pack-group=2 fit.py --sampfile parameters_small.h5 --samp $NSAMP --selfile selected.h5 --nsel $NSEL --chainfile population_small_H0.h5 --tracefile traceplot_small_H0.pdf --prior H0
-srun --pack-group=3 fit.py --sampfile parameters_small.h5 --samp $NSAMP --selfile selected.h5 --nsel $NSEL --chainfile population_small_H0_Planck_Om.h5 --tracefile traceplot_small_H0_Planck_Om.pdf --prior H0-Planck-Om
+srun fit.py --sampfile parameters_small.h5 --samp $NSAMP --selfile selected.h5 --nsel $NSEL --chainfile population_small_free.h5 --tracefile traceplot_small_free.pdf --prior free &
+srun fit.py --sampfile parameters_small.h5 --samp $NSAMP --selfile selected.h5 --nsel $NSEL --chainfile population_small_Planck_Om_w.h5 --tracefile traceplot_small_Planck_Om_w.pdf --prior Planck-Om-w &
+srun fit.py --sampfile parameters_small.h5 --samp $NSAMP --selfile selected.h5 --nsel $NSEL --chainfile population_small_H0.h5 --tracefile traceplot_small_H0.pdf --prior H0 &
+srun fit.py --sampfile parameters_small.h5 --samp $NSAMP --selfile selected.h5 --nsel $NSEL --chainfile population_small_H0_Planck_Om.h5 --tracefile traceplot_small_H0_Planck_Om.pdf --prior H0-Planck-Om &
+
+wait
