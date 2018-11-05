dd218f24a1b15bd958ad268b514ab8a39b107c5d
Will M. Farr <wfarr-vscholar@flatironinstitute.org>
2018-11-04 22:06:31 -0500

    Record the number of selected samples in integral approximation.

--- fit.py
+++ fit.py
@@ -99,6 +99,7 @@ savefig(args.tracefile)
 
 with h5py.File(args.chainfile, 'w') as out:
     out.attrs['nsamp'] = nsamp
+    out.attrs['nsel'] = ndet
 
     for n in ['H0', 'Om', 'w', 'R0', 'MMin', 'MMax', 'sigma_low', 'sigma_high', 'alpha', 'beta', 'gamma', 'Nex', 'neff_det', ]:
         out.create_dataset(n, data=fit[n], compression='gzip', shuffle=True)
