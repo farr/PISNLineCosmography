6cc0ac5631eb6b079807174004671ca17ffa3113
Will M. Farr <wfarr-vscholar@flatironinstitute.org>
2018-11-13 13:39:56 -0500

    More samples in small and 1-year convergence runs, extrapolate to five-year.

--- run_zerou.sh
+++ run_zerou.sh
@@ -7,15 +7,15 @@ source ~/.bashrc
 source activate
 export PYTHONPATH="$PYTHONPATH:/mnt/home/wfarr/PISNLineCosmography"
 
-NSEL=4096
+NSEL=16384
 ./fit.py --sampfile observations.h5 --subset small --zero-uncert --selfile selected.h5 --nsel $NSEL --chainfile population_small_zerou.h5 --tracefile traceplot_small_zerou.pdf > logs/small_zerou.out 2> logs/small_zerou.err &
 ./fit.py --cosmo-constraints --sampfile observations.h5 --subset small --zero-uncert --selfile selected.h5 --nsel $NSEL --chainfile population_small_cosmo_zerou.h5 --tracefile traceplot_small_cosmo_zerou.pdf > logs/small_cosmo_zerou.out 2> logs/small_cosmo_zerou.err &
 
-NSEL=65536
+NSEL=524288
 ./fit.py --sampfile observations.h5 --subset 1yr --zero-uncert --selfile selected.h5 --nsel $NSEL --chainfile population_1yr_zerou.h5 --tracefile traceplot_1yr_zerou.pdf > logs/1yr_zerou.out 2>logs/1yr_zerou.err &
 ./fit.py --cosmo-constraints --sampfile observations.h5 --subset 1yr --zero-uncert --selfile selected.h5 --nsel $NSEL --chainfile population_1yr_cosmo_zerou.h5 --tracefile traceplot_1yr_cosmo_zerou.pdf > logs/1yr_cosmo_zerou.out 2>logs/1yr_cosmo_zerou.err &
 
-NSEL=524288
+NSEL=4194304
 ./fit.py --sampfile observations.h5 --zero-uncert --selfile selected.h5 --nsel $NSEL --chainfile population_5yr_zerou.h5 --tracefile traceplot_5yr_zerou.pdf > logs/5yr_zerou.out 2> logs/5yr_zerou.err &
 ./fit.py --cosmo-constraints --sampfile observations.h5 --zero-uncert --selfile selected.h5 --nsel $NSEL --chainfile population_5yr_cosmo_zerou.h5 --tracefile traceplot_5yr_cosmo_zerou.pdf > logs/5yr_cosmo_zerou.out 2> logs/5yr_cosmo_zerou.err &
 
--- run_zerou_check.sh
+++ run_zerou_check.sh
@@ -7,15 +7,15 @@ source ~/.bashrc
 source activate
 export PYTHONPATH="$PYTHONPATH:/mnt/home/wfarr/PISNLineCosmography"
 
-NSEL=2048
+NSEL=8192
 ./fit.py --sampfile observations.h5 --subset small --zero-uncert --selfile selected.h5 --nsel $NSEL --chainfile population_small_zerou_check.h5 --tracefile traceplot_small_zerou_check.pdf > logs/small_zerou_check.out 2> logs/small_zerou_check.err &
 ./fit.py --cosmo-constraints --sampfile observations.h5 --subset small --zero-uncert --selfile selected.h5 --nsel $NSEL --chainfile population_small_cosmo_zerou_check.h5 --tracefile traceplot_small_cosmo_zerou_check.pdf > logs/small_cosmo_zerou_check.out 2> logs/small_cosmo_zerou_check.err &
 
-NSEL=32768
+NSEL=262144
 ./fit.py --sampfile observations.h5 --subset 1yr --zero-uncert --selfile selected.h5 --nsel $NSEL --chainfile population_1yr_zerou_check.h5 --tracefile traceplot_1yr_zerou_check.pdf > logs/1yr_zerou_check.out 2>logs/1yr_zerou_check.err &
 ./fit.py --cosmo-constraints --sampfile observations.h5 --subset 1yr --zero-uncert --selfile selected.h5 --nsel $NSEL --chainfile population_1yr_cosmo_zerou_check.h5 --tracefile traceplot_1yr_cosmo_zerou_check.pdf > logs/1yr_cosmo_zerou_check.out 2>logs/1yr_cosmo_zerou_check.err &
 
-NSEL=262144
+NSEL=2097152
 ./fit.py --sampfile observations.h5 --zero-uncert --selfile selected.h5 --nsel $NSEL --chainfile population_5yr_zerou_check.h5 --tracefile traceplot_5yr_zerou_check.pdf > logs/5yr_zerou_check.out 2> logs/5yr_zerou_check.err &
 ./fit.py --cosmo-constraints --sampfile observations.h5 --zero-uncert --selfile selected.h5 --nsel $NSEL --chainfile population_5yr_cosmo_zerou_check.h5 --tracefile traceplot_5yr_cosmo_zerou_check.pdf > logs/5yr_cosmo_zerou_check.out 2> logs/5yr_cosmo_zerou_check.err &
 
