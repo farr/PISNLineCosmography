f70bb2b9e846181ced1a70c6621fbc78df11cd1b
Will M. Farr <wfarr-vscholar@flatironinstitute.org>
2018-11-14 10:14:31 -0500

    More selection function samples in 1yr and 5yr runs.

--- run.sh
+++ run.sh
@@ -13,12 +13,12 @@ NSEL=8192
 ./fit.py --cosmo-constraints --sampfile observations.h5 --subset small --samp $NSAMP --selfile selected.h5 --nsel $NSEL --chainfile population_small_cosmo.h5 --tracefile traceplot_small_cosmo.pdf > logs/small_cosmo.out 2> logs/small_cosmo.err &
 
 NSAMP=128
-NSEL=131072
+NSEL=262144
 ./fit.py --sampfile observations.h5 --subset 1yr --samp $NSAMP --selfile selected.h5 --nsel $NSEL --chainfile population_1yr.h5 --tracefile traceplot_1yr.pdf > logs/1yr.out 2>logs/1yr.err &
 ./fit.py --cosmo-constraints --sampfile observations.h5 --subset 1yr --samp $NSAMP --selfile selected.h5 --nsel $NSEL --chainfile population_1yr_cosmo.h5 --tracefile traceplot_1yr_cosmo.pdf > logs/1yr_cosmo.out 2>logs/1yr_cosmo.err &
 
 NSAMP=128
-NSEL=524288
+NSEL=1048576
 ./fit.py --sampfile observations.h5 --samp $NSAMP --selfile selected.h5 --nsel $NSEL --chainfile population_5yr.h5 --tracefile traceplot_5yr.pdf > logs/5yr.out 2> logs/5yr.err &
 ./fit.py --cosmo-constraints --sampfile observations.h5 --samp $NSAMP --selfile selected.h5 --nsel $NSEL --chainfile population_5yr_cosmo.h5 --tracefile traceplot_5yr_cosmo.pdf > logs/5yr_cosmo.out 2> logs/5yr_cosmo.err &
 
