05d4116c7eb2235d2d51dbc701457827645f433b
Will M. Farr <wfarr-vscholar@flatironinstitute.org>
2018-11-13 22:10:53 -0500

    More samples in zerou.sh for 1 and 5 year.  (Bummer.)

--- run_zerou.sh
+++ run_zerou.sh
@@ -11,11 +11,11 @@ NSEL=8192
 ./fit.py --sampfile observations.h5 --subset small --zero-uncert --selfile selected.h5 --nsel $NSEL --chainfile population_small_zerou.h5 --tracefile traceplot_small_zerou.pdf > logs/small_zerou.out 2> logs/small_zerou.err &
 ./fit.py --cosmo-constraints --sampfile observations.h5 --subset small --zero-uncert --selfile selected.h5 --nsel $NSEL --chainfile population_small_cosmo_zerou.h5 --tracefile traceplot_small_cosmo_zerou.pdf > logs/small_cosmo_zerou.out 2> logs/small_cosmo_zerou.err &
 
-NSEL=131072
+NSEL=1048576
 ./fit.py --sampfile observations.h5 --subset 1yr --zero-uncert --selfile selected.h5 --nsel $NSEL --chainfile population_1yr_zerou.h5 --tracefile traceplot_1yr_zerou.pdf > logs/1yr_zerou.out 2>logs/1yr_zerou.err &
 ./fit.py --cosmo-constraints --sampfile observations.h5 --subset 1yr --zero-uncert --selfile selected.h5 --nsel $NSEL --chainfile population_1yr_cosmo_zerou.h5 --tracefile traceplot_1yr_cosmo_zerou.pdf > logs/1yr_cosmo_zerou.out 2>logs/1yr_cosmo_zerou.err &
 
-NSEL=524288
+NSEL=8388608
 ./fit.py --sampfile observations.h5 --zero-uncert --selfile selected.h5 --nsel $NSEL --chainfile population_5yr_zerou.h5 --tracefile traceplot_5yr_zerou.pdf > logs/5yr_zerou.out 2> logs/5yr_zerou.err &
 ./fit.py --cosmo-constraints --sampfile observations.h5 --zero-uncert --selfile selected.h5 --nsel $NSEL --chainfile population_5yr_cosmo_zerou.h5 --tracefile traceplot_5yr_cosmo_zerou.pdf > logs/5yr_cosmo_zerou.out 2> logs/5yr_cosmo_zerou.err &
 
