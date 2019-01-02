cab310e62ea70a0a6bffb5862125ccfd9aa78aea
Will M. Farr <wfarr-vscholar@flatironinstitute.org>
2019-01-02 17:14:07 -0500

    Small runs on cluster done.

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -n 1 --ntasks 3 --cpus-per-task 4 -p cca -o logs/run.out -e logs/run.err
+#SBATCH -n 1 --ntasks 3 --cpus-per-task 4 -t 14-00:00:00 -o logs/run.out -e logs/run.err
 
 set -e
 
