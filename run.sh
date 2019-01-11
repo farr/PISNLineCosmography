4ea27457076aa9b88117dcaa235a55fc2cdb3908
Will M. Farr <wfarr-vscholar@flatironinstitute.org>
2019-01-11 15:11:40 -0500

    More posterior samples in short runs.

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ source activate
 export PYTHONPATH="$PYTHONPATH:/mnt/home/wfarr/PISNLineCosmography"
 
 NSEL=4096
-./fit.py --sampfile observations.h5 --subset small --selfile selected.h5 --nsel $NSEL --chainfile population_small.h5 --tracefile traceplot_small.pdf > logs/small.out 2>&1 &
+./fit.py --sampfile observations.h5 --nsamp 512 --subset small --selfile selected.h5 --nsel $NSEL --chainfile population_small.h5 --tracefile traceplot_small.pdf > logs/small.out 2>&1 &
 
 NSEL=65536
 ./fit.py --sampfile observations.h5 --subset 1yr --selfile selected.h5 --nsel $NSEL --chainfile population_1yr.h5 --tracefile traceplot_1yr.pdf > logs/1yr.out 2>&1 &
