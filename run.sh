c256cf814b51bf5ca79b7f82babff69d3945bb7b
Will M. Farr <wfarr-vscholar@flatironinstitute.org>
2019-01-31 17:20:22 -0500

    Combining doesn't work---too few effective samples in 10D---so back to 5-year runs.

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -n 1 --ntasks 4 --cpus-per-task 4 -p cca -o logs/run.out -e logs/run.err
+#SBATCH -n 1 --ntasks 6 --cpus-per-task 4 -p cca -o logs/run.out -e logs/run.err
 
 set -e
 
@@ -14,5 +14,9 @@ NSEL=4096
 NSEL=32768
 ./fit.py --sampfile observations.h5 --nsamp nsamp_1yr.txt --subset 1yr --selfile selected.h5 --nsel $NSEL --chainfile population_1yr.h5 --tracefile traceplot_1yr.pdf > logs/1yr.out 2>&1 &
 ./fit.py --cosmo-prior --sampfile observations.h5 --nsamp nsamp_1yr.txt --subset 1yr --selfile selected.h5 --nsel $NSEL --chainfile population_1yr_cosmo.h5 --tracefile traceplot_1yr_cosmo.pdf > logs/1yr_cosmo.out 2>&1 &
+
+NSEL=131072
+./fit.py --sampfile observations.h5 --nsamp nsamp_5yr.txt --selfile selected.h5 --nsel $NSEL --chainfile population_5yr.h5 --tracefile traceplot_5yr.pdf > logs/5yr.out 2>&1 &
+./fit.py --cosmo-prior --sampfile observations.h5 --nsamp nsamp_5yr.txt --selfile selected.h5 --nsel $NSEL --chainfile population_5yr_cosmo.h5 --tracefile traceplot_5yr_cosmo.pdf > logs/5yr_cosmo.out 2>&1 &
 
 wait
