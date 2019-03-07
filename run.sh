085dc8c0832f7d61e9ee20ddac8ce6f0d64e78c4
Will M. Farr <wfarr-vscholar@flatironinstitute.org>
2019-03-06 22:05:48 -0500

    More selection integral samples for 1 year and 5 year runs.

--- run.sh
+++ run.sh
@@ -11,11 +11,11 @@ NSEL=4096
 ./fit.py --sampfile observations.h5 --nsamp 128 --subset small --selfile selected.h5 --nsel $NSEL --chainfile population_small.h5 --tracefile traceplot_small.pdf > logs/small.out 2>&1 &
 ./fit.py --cosmo-prior --sampfile observations.h5 --nsamp 128 --subset small --selfile selected.h5 --nsel $NSEL --chainfile population_small_cosmo.h5 --tracefile traceplot_small_cosmo.pdf > logs/small_cosmo.out 2>&1 &
 
-NSEL=32768
+NSEL=65536
 ./fit.py --sampfile observations.h5 --nsamp 128 --subset 1yr --selfile selected.h5 --nsel $NSEL --chainfile population_1yr.h5 --tracefile traceplot_1yr.pdf > logs/1yr.out 2>&1 &
 ./fit.py --cosmo-prior --sampfile observations.h5 --nsamp 128 --subset 1yr --selfile selected.h5 --nsel $NSEL --chainfile population_1yr_cosmo.h5 --tracefile traceplot_1yr_cosmo.pdf > logs/1yr_cosmo.out 2>&1 &
 
-NSEL=131072
+NSEL=262144
 ./fit.py --sampfile observations.h5 --nsamp 128 --selfile selected.h5 --nsel $NSEL --chainfile population_5yr.h5 --tracefile traceplot_5yr.pdf > logs/5yr.out 2>&1 &
 ./fit.py --cosmo-prior --sampfile observations.h5 --nsamp 128 --selfile selected.h5 --nsel $NSEL --chainfile population_5yr_cosmo.h5 --tracefile traceplot_5yr_cosmo.pdf > logs/5yr_cosmo.out 2>&1 &
 
