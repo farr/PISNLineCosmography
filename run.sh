77f640348ed61e09cd29e4abd0e62b07b359dcd6
Will M. Farr <wfarr-vscholar@flatironinstitute.org>
2019-03-13 20:31:14 -0400

    Don't reject samples for too-inaccurate selection function.

--- run.sh
+++ run.sh
@@ -7,16 +7,18 @@ source ~/.bashrc
 source activate
 export PYTHONPATH="$PYTHONPATH:/mnt/home/wfarr/PISNLineCosmography"
 
+NSAMP=32
+
+NSEL=1024
+./fit.py --sampfile observations.h5 --nsamp $NSAMP --subset small --selfile selected.h5 --nsel $NSEL --chainfile population_small.h5 --tracefile traceplot_small.pdf > logs/small.out 2>&1 &
+./fit.py --cosmo-prior --sampfile observations.h5 --nsamp $NSAMP --subset small --selfile selected.h5 --nsel $NSEL --chainfile population_small_cosmo.h5 --tracefile traceplot_small_cosmo.pdf > logs/small_cosmo.out 2>&1 &
+
 NSEL=4096
-./fit.py --sampfile observations.h5 --nsamp 128 --subset small --selfile selected.h5 --nsel $NSEL --chainfile population_small.h5 --tracefile traceplot_small.pdf > logs/small.out 2>&1 &
-./fit.py --cosmo-prior --sampfile observations.h5 --nsamp 128 --subset small --selfile selected.h5 --nsel $NSEL --chainfile population_small_cosmo.h5 --tracefile traceplot_small_cosmo.pdf > logs/small_cosmo.out 2>&1 &
+./fit.py --sampfile observations.h5 --nsamp $NSAMP --subset 1yr --selfile selected.h5 --nsel $NSEL --chainfile population_1yr.h5 --tracefile traceplot_1yr.pdf > logs/1yr.out 2>&1 &
+./fit.py --cosmo-prior --sampfile observations.h5 --nsamp $NSAMP --subset 1yr --selfile selected.h5 --nsel $NSEL --chainfile population_1yr_cosmo.h5 --tracefile traceplot_1yr_cosmo.pdf > logs/1yr_cosmo.out 2>&1 &
 
-NSEL=65536
-./fit.py --sampfile observations.h5 --nsamp 128 --subset 1yr --selfile selected.h5 --nsel $NSEL --chainfile population_1yr.h5 --tracefile traceplot_1yr.pdf > logs/1yr.out 2>&1 &
-./fit.py --cosmo-prior --sampfile observations.h5 --nsamp 128 --subset 1yr --selfile selected.h5 --nsel $NSEL --chainfile population_1yr_cosmo.h5 --tracefile traceplot_1yr_cosmo.pdf > logs/1yr_cosmo.out 2>&1 &
-
-NSEL=262144
-./fit.py --sampfile observations.h5 --nsamp 128 --selfile selected.h5 --nsel $NSEL --chainfile population_5yr.h5 --tracefile traceplot_5yr.pdf > logs/5yr.out 2>&1 &
-./fit.py --cosmo-prior --sampfile observations.h5 --nsamp 128 --selfile selected.h5 --nsel $NSEL --chainfile population_5yr_cosmo.h5 --tracefile traceplot_5yr_cosmo.pdf > logs/5yr_cosmo.out 2>&1 &
+NSEL=32768
+./fit.py --sampfile observations.h5 --nsamp $NSAMP --selfile selected.h5 --nsel $NSEL --chainfile population_5yr.h5 --tracefile traceplot_5yr.pdf > logs/5yr.out 2>&1 &
+./fit.py --cosmo-prior --sampfile observations.h5 --nsamp $NSAMP --selfile selected.h5 --nsel $NSEL --chainfile population_5yr_cosmo.h5 --tracefile traceplot_5yr_cosmo.pdf > logs/5yr_cosmo.out 2>&1 &
 
 wait
