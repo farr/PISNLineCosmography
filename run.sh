d8541d8de0221d6d265ca01391d788bf4867d4bb
Will M. Farr <wfarr-vscholar@flatironinstitute.org>
2019-03-26 15:36:08 -0400

    Can get away with fewer samples for 5 years runs, too (probably?).

--- run.sh
+++ run.sh
@@ -17,7 +17,7 @@ NSEL=32768
 ./fit.py --sampfile observations.h5 --nsamp $NSAMP --subset 1yr --selfile selected.h5 --nsel $NSEL --chainfile population_1yr.h5 --tracefile traceplot_1yr.pdf > logs/1yr.out 2>&1 &
 ./fit.py --cosmo-prior --sampfile observations.h5 --nsamp $NSAMP --subset 1yr --selfile selected.h5 --nsel $NSEL --chainfile population_1yr_cosmo.h5 --tracefile traceplot_1yr_cosmo.pdf > logs/1yr_cosmo.out 2>&1 &
 
-NSEL=262144
+NSEL=131072
 ./fit.py --sampfile observations.h5 --nsamp $NSAMP --selfile selected.h5 --nsel $NSEL --chainfile population_5yr.h5 --tracefile traceplot_5yr.pdf > logs/5yr.out 2>&1 &
 ./fit.py --cosmo-prior --sampfile observations.h5 --nsamp $NSAMP --selfile selected.h5 --nsel $NSEL --chainfile population_5yr_cosmo.h5 --tracefile traceplot_5yr_cosmo.pdf > logs/5yr_cosmo.out 2>&1 &
 
