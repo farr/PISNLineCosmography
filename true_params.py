a7744823cbe88b17241d4836e7f5f0eea8426233
Will M. Farr <wfarr-vscholar@flatironinstitute.org>
2019-04-10 17:13:17 -0400

    New observations, with sharp cutoff, and better posterior sampling (semi-analytic).

--- true_params.py
+++ true_params.py
@@ -5,20 +5,18 @@ import astropy.units as u
 
 true_params = {
     'R0': 60.0, # Gpc^-3 yr^-1
-    'R0_30': 43.88, # R_{0,30} / 30^2 = R_0 p\left( m_1 = 30, m_2 = 30 \right) (to avoid having to normalize the PDF every time)
+    'R0_30': 64.4468, # R_{0,30} / 30^2 = R_0 p\left( m_1 = 30, m_2 = 30 \right) (to avoid having to normalize the PDF every time)
     'alpha': 0.75,
     'beta': 0.0,
     'gamma': 3.0,
     'MMin': 5.0, # MSun
-    'MMax': 40.0, # MSun
+    'MMax': 45.0, # MSun
     'H0': Planck15.H0.to(u.km/u.s/u.Mpc).value, # km/s/Mpc
     'Om': Planck15.Om0,
     'w': -1.0,
     'z_p': 0.75,
     'w_p': -1.0,
-    'w_a': 0.0,
-    'sigma_min': 0.1,
-    'sigma_max': 0.1
+    'w_a': 0.0
 }
 
 uncert = {
