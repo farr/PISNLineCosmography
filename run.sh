468ac1a6a934f072008b43ba3ee117ee056fa5f6
Will M. Farr <wfarr-vscholar@flatironinstitute.org>
2019-04-24 21:04:16 -0400

    Saving run settings in run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -n 1 --ntasks 6 --cpus-per-task 4 -p gen -t 14-00:00:00 -o logs/run.out -e logs/run.err
+#SBATCH -n 1 --ntasks 6 --cpus-per-task 4 -p gen -t 21-00:00:00 -o logs/run.out -e logs/run.err
 
 set -e
 
@@ -12,11 +12,11 @@ NSEL=4096
 ./fit.py --cosmo-prior --sampfile observations.h5 --subset small --selfile selected.h5 --nsel $NSEL --chainfile population_small_cosmo.h5 --tracefile traceplot_small_cosmo.pdf > logs/small_cosmo.out 2>&1 &
 
 NSEL=32768
-./fit.py --sampfile observations.h5 --subset 1yr --selfile selected.h5 --nsel $NSEL --chainfile population_1yr.h5 --tracefile traceplot_1yr.pdf > logs/1yr.out 2>&1 &
-./fit.py --cosmo-prior --sampfile observations.h5 --subset 1yr --selfile selected.h5 --nsel $NSEL --chainfile population_1yr_cosmo.h5 --tracefile traceplot_1yr_cosmo.pdf > logs/1yr_cosmo.out 2>&1 &
+./fit.py --iter 8000 --thin 8 --sampfile observations.h5 --subset 1yr --selfile selected.h5 --nsel $NSEL --chainfile population_1yr.h5 --tracefile traceplot_1yr.pdf > logs/1yr.out 2>&1 &
+./fit.py --iter 8000 --thin 8 --cosmo-prior --sampfile observations.h5 --subset 1yr --selfile selected.h5 --nsel $NSEL --chainfile population_1yr_cosmo.h5 --tracefile traceplot_1yr_cosmo.pdf > logs/1yr_cosmo.out 2>&1 &
 
 NSEL=131072
-./fit.py --sampfile observations.h5 --selfile selected.h5 --nsel $NSEL --chainfile population_5yr.h5 --tracefile traceplot_5yr.pdf > logs/5yr.out 2>&1 &
+./fit.py --iter 2000 --thin 2 --sampfile observations.h5 --selfile selected.h5 --nsel $NSEL --chainfile population_5yr.h5 --tracefile traceplot_5yr.pdf > logs/5yr.out 2>&1 &
 ./fit.py --cosmo-prior --sampfile observations.h5 --selfile selected.h5 --nsel $NSEL --chainfile population_5yr_cosmo.h5 --tracefile traceplot_5yr_cosmo.pdf > logs/5yr_cosmo.out 2>&1 &
 
 wait
