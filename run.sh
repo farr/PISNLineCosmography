951df167d1379acfd830ee2d7be768b60050b1b7
Will M. Farr <wfarr-vscholar@flatironinstitute.org>
2019-05-06 20:21:48 -0400

    More samples for 5 year run.

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ NSEL=32768
 ./fit.py --iter 8000 --thin 8 --cosmo-prior --sampfile observations.h5 --subset 1yr --selfile selected.h5 --nsel $NSEL --chainfile population_1yr_cosmo.h5 --tracefile traceplot_1yr_cosmo.pdf > logs/1yr_cosmo.out 2>&1 &
 
 NSEL=131072
-./fit.py --iter 2000 --thin 2 --sampfile observations.h5 --selfile selected.h5 --nsel $NSEL --chainfile population_5yr.h5 --tracefile traceplot_5yr.pdf > logs/5yr.out 2>&1 &
+./fit.py --iter 4000 --thin 2 --sampfile observations.h5 --selfile selected.h5 --nsel $NSEL --chainfile population_5yr.h5 --tracefile traceplot_5yr.pdf > logs/5yr.out 2>&1 &
 ./fit.py --cosmo-prior --sampfile observations.h5 --selfile selected.h5 --nsel $NSEL --chainfile population_5yr_cosmo.h5 --tracefile traceplot_5yr_cosmo.pdf > logs/5yr_cosmo.out 2>&1 &
 
 wait
