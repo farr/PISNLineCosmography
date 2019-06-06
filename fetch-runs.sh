d86c3cbe54ac9ccf96101dbb7721bf0741581bf1
Will M. Farr <wfarr-vscholar@flatironinstitute.org>
2019-06-05 21:53:50 -0400

    Removed old runs in favour of new, smooth runs.

--- fetch-runs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e 
-
-rsync -e ssh -avz --progress --include="**/*.h5" --include="**/*.pdf" --include="**/*.out" --include="**/logs" --include="**/GWTC-1" --exclude="**" rusty:~/PISN*/ ./
