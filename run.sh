31f8555bc9a65f8ff4b2423848d759308c595358
Will M. Farr <wfarr-vscholar@flatironinstitute.org>
2019-07-12 00:31:30 -0400

    Ready to do 1 and 5 year constrained cosmology runs.

--- run.sh
+++ run.sh
@@ -16,9 +16,11 @@ NSEL=32768
 ./fit.py --sampfile observations.h5 --subset 1yr --selfile selected.h5 --nsel $NSEL --chainfile population_1yr_$NSEL.nc --tracefile traceplot_1yr_$NSEL.pdf > logs/1yr_$NSEL.out 2>&1 &
 NSEL=65536
 ./fit.py --sampfile observations.h5 --subset 1yr --selfile selected.h5 --nsel $NSEL --chainfile population_1yr_$NSEL.nc --tracefile traceplot_1yr_$NSEL.pdf > logs/1yr_$NSEL.out 2>&1 &
+#./fit.py --cosmo-prior --sampfile observations.h5 --subset 1yr --selfile selected.h5 --nsel $NSEL --chainfile population_1yr_cosmo_$NSEL.nc --tracefile traceplot_1yr_cosmo_$NSEL.pdf > logs/1yr_cosmo_$NSEL.out 2>&1 &
 
 NSEL=131072
 ./fit.py --sampfile observations.h5 --selfile selected.h5 --nsel $NSEL --chainfile population_5yr_$NSEL.nc --tracefile traceplot_5yr_$NSEL.pdf > logs/5yr_$NSEL.out 2>&1 &
+#./fit.py --cosmo-prior --sampfile observations.h5 --selfile selected.h5 --nsel $NSEL --chainfile population_5yr_cosmo_$NSEL.nc --tracefile traceplot_5yr_cosmo_$NSEL.pdf > logs/5yr_cosmo_$NSEL.out 2>&1 &
 NSEL=262144
 ./fit.py --sampfile observations.h5 --selfile selected.h5 --nsel $NSEL --chainfile population_5yr_$NSEL.nc --tracefile traceplot_5yr_$NSEL.pdf > logs/5yr_$NSEL.out 2>&1 &
 
